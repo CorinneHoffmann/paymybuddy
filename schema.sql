921a48d6896ce25b6adf5a53494d0129f436ce3f
Corinne Hoffmann <corinne.hoffmann@axa.fr>
2020-05-14 12:33:18 +0200

    depot des livrables

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
+use paymybuddy;
+
+DROP TABLE IF EXISTS commission;
+DROP TABLE IF EXISTS operationcompte;
+DROP TABLE IF EXISTS comptebancaire;
 DROP TABLE IF EXISTS compte;
 DROP TABLE IF EXISTS personne_amis;
 DROP TABLE IF EXISTS personne;
@@ -13,19 +18,11 @@ CREATE TABLE personne (
 )
 ENGINE=INNODB;
 
-CREATE TABLE compte (
-  idcompte bigint(20) NOT NULL AUTO_INCREMENT,
-  solde  decimal(8,2) NOT NULL,
-  personneid bigint(20) NOT NULL,
-  PRIMARY KEY (idcompte),
-  FOREIGN KEY (personneid) REFERENCES personne(idpersonne)
-)
-ENGINE=INNODB;
-
 CREATE TABLE personne_amis (
   personne_idpersonne bigint(20) NOT NULL,
   amis_idpersonne bigint(20) NOT NULL,
-  FOREIGN KEY (personne_idpersonne) REFERENCES personne(idpersonne)
+  PRIMARY KEY (personne_idpersonne,amis_idpersonne),
+  FOREIGN KEY (personne_idpersonne) REFERENCES personne(idpersonne),
   FOREIGN KEY (amis_idpersonne) REFERENCES personne(idpersonne)
 )
 ENGINE=INNODB;
@@ -38,5 +35,40 @@ CREATE TABLE comptebancaire (
   personneid bigint(20) NOT NULL,
   PRIMARY KEY (idcomptebancaire),
   FOREIGN KEY (personneid) REFERENCES personne(idpersonne)
+)
+ENGINE=INNODB;
+
+CREATE TABLE compte (
+  idcompte bigint(20) NOT NULL AUTO_INCREMENT,
+  solde  double NOT NULL,
+  personneid bigint(20) NOT NULL,
+  PRIMARY KEY (idcompte),
+  FOREIGN KEY (personneid) REFERENCES personne(idpersonne)
+)
+ENGINE=INNODB;
+
+CREATE TABLE operationcompte (
+  idoperationcompte bigint(20) NOT NULL AUTO_INCREMENT,
+  dateoperation  date NOT NULL,
+  debitcredit  varchar(1) NOT NULL,
+  montant  double NOT NULL,
+  typeoperation  varchar(10) NOT NULL,
+  compteid bigint(20), 
+  comptebancaireid bigint(20), 
+  personneid bigint(20),
+  PRIMARY KEY (idoperationcompte),
+  FOREIGN KEY (compteid) REFERENCES compte(idcompte),
+  FOREIGN KEY (comptebancaireid) REFERENCES comptebancaire(idcomptebancaire),
+  FOREIGN KEY (personneid) REFERENCES personne(idpersonne)
+)
+ENGINE=INNODB;
+
+CREATE TABLE commission (
+  idcommission bigint(20) NOT NULL AUTO_INCREMENT,
+  montant  double NOT NULL,
+  taux  double NOT NULL,
+  operationcompteid bigint(20), 
+  PRIMARY KEY (idcommission),
+  FOREIGN KEY (operationcompteid) REFERENCES operationcompte(idoperationcompte)
 )
 ENGINE=INNODB;
